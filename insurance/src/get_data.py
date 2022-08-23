9c76455bb00d2930c7f6db9220c438bd34d45b12
yashpanchal089 <panchalyash089@gmail.com>
2022-08-23 16:51:49 +0530

    five commit

--- insurance/src/get_data.py
+++ insurance/src/get_data.py
@@ -17,7 +17,8 @@ def get_data(config_path):
     #print(config)
     data_path=config["data_source"]["s3_source"]
     df=pd.read_csv(data_path, sep="," ,encoding="utf-8")
-    print(df.head())
+    #print(df.head())
+    return df
 
 if __name__=="__main__":
     args=argparse.ArgumentParser()
---
+++ insurance/src/split_data.py
@@ -0,0 +1,34 @@
+from distutils.command.config import config
+from email import header
+import encodings
+import os
+# from insurance.src.get_data import read_params
+import yaml
+import pandas as pd
+import numpy as np
+import argparse
+from pkgutil import get_data
+import configparser
+from get_data import read_params, get_data
+from sklearn.model_selection import train_test_split
+
+def split_data(config_path):
+    config=read_params(config_path)
+    test_data_path=config["split_data"]["test_path"]
+    train_data_path=config["split_data"]["train_path"]
+    raw_data_path=config["load_data"]["raw_dataset_csv"]
+    split_ratio=config["split_data"]["test_size"]
+    random_state=config["base"]["random_state"]
+    
+    df = pd.read_csv(raw_data_path, sep=",")
+    train, test=train_test_split(df, test_size=split_ratio, random_state=random_state)
+
+    train.to_csv(train_data_path, sep=",", index=False, encoding="utf-8")
+    test.to_csv(test_data_path, sep=",", index=False, encoding="utf-8")
+
+
+if __name__=="__main__":
+    args=argparse.ArgumentParser()
+    args.add_argument("--config", default="params.yaml")
+    parsed_args=args.parse_args()
+    split_data(config_path=parsed_args.config)
